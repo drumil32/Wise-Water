0564ca8776ac0a3bb8f9c3292fd7feb8d1823dc8
Drumil_Akhenia <akheniad@gmail.com>
2022-12-23 21:20:11 +0530

    minor

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const app = express();
+const app = express()
 const connectDB = require('./config/db');
 const cors = require('cors');
 const dotenv = require('dotenv').config();
