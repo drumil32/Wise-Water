46b257cea20793ca85f1259b3e75e52570847195
Drumil_Akhenia <akheniad@gmail.com>
2022-12-26 22:28:29 +0530

    tried to get user location

---
+++ backend/models/tempModel.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const tempSchema = mongoose.Schema({
+    latitude : {
+        type : Number,
+    }
+    ,
+    longitude : {
+        type : Number,
+    }
+});
+
+
+
+module.exports = mongoose.model("Temp",tempSchema);
