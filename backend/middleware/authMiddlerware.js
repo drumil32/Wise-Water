c59859051c932fbaf9f16e23982c7a35bd3b5e9f
Drumil_Akhenia <akheniad@gmail.com>
2023-01-07 12:09:54 +0530

    formatted

--- backend/middleware/authMiddlerware.js
+++ backend/middleware/authMiddlerware.js
@@ -1,16 +1,16 @@
 const asyncHandler = require('express-async-handler');
-const {mapCollectionName} = require('../utility/mappingCollection');
-const {decodeJWTtoken} = require('../utility/decodeJWTtoken');
+const { mapCollectionName } = require('../utility/mappingCollection');
+const { decodeJWTtoken } = require('../utility/decodeJWTtoken');
 
 const protect = asyncHandler(async (req, res, next) => {
     console.log(req.body);
-    console.log( 'from protect');
-    const decoded = decodeJWTtoken(req,res);
-    
+    console.log('from protect');
+    const decoded = decodeJWTtoken(req, res);
+
     try {
-        const collection = mapCollectionName( decoded.collectionName );
-        req.user = await collection.findById(decoded.id).select('-password'); 
-        
+        const collection = mapCollectionName(decoded.collectionName);
+        req.user = await collection.findById(decoded.id).select('-password');
+
         console.log(req.user)
         console.log('from protect is done');
         next();
